9f33e581ae3ecbcfedf5e06597b3cc3512d43264
CYvoll <christoffy@uia.no>
2026-01-07 15:34:53 +0100

    for loop

--- baby-steps.js
+++ baby-steps.js
@@ -1,1 +1,7 @@
-console.log(+process.argv[2])
+
+let total = 0;
+for (let i = 2; i < process.argv.length; i++) {
+  console.log(+process.argv[i]);
+  total += +process.argv[i];  
+}
+console.log (total);
